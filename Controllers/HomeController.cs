6d4a628e6a6d90ff1337606b66a3f69092bab3c1
usa1991 <usmanjack1991@gmail.com>
2026-07-12 15:00:44 +0530

    changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,18 +7,6 @@ public class HomeController : Controller
     public IActionResult Index() => View();
     public IActionResult About() => View();
     public IActionResult Vision() => View();
-    public IActionResult Teachers() => View();
-    public IActionResult Courses() => View();
-    public IActionResult Timetable() => View();
-    public IActionResult Admissions() => View();
-    public IActionResult Fees() => View();
-    public IActionResult News() => View();
-    public IActionResult Gallery() => View();
-    public IActionResult Events() => View();
-    public IActionResult Faq() => View();
     public IActionResult Contact() => View();
-    public IActionResult Downloads() => View();
-    public IActionResult Library() => View();
-    public IActionResult Prayer() => View();
-    public IActionResult Knowledge() => View();
+    public IActionResult Privacy() => View();
 }
